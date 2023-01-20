71325dbc3e4010f91ae604c65c673e4fe78fcd67
Kimberllin Martins <kimberllin.m@gmail.com>
2023-01-19 21:49:57 -0300

    Create dia_dois.js

---
+++ dia_dois.js
@@ -0,0 +1,4 @@
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
+alert(`"Olá ${nome}, você tem ${idade} anos e está aprendendo ${linguagem}!"`);
